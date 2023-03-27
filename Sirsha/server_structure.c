7eaec9c34be9615105b2d83e5afb1d6d3fe6c58c
Sirsha Dey <69218962+Sirsha2468@users.noreply.github.com>
2023-03-27 11:46:26 +0530

    Update server_structure.c

--- Sirsha/server_structure.c
+++ Sirsha/server_structure.c
@@ -1,10 +1,10 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <stdio.h>
-#include <sys/un.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdio.h>
+#include<sys/un.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
 
 typedef struct
 {
@@ -23,35 +23,35 @@ float cgpa;
 
 int main()
 {
-   int sockfd;
-   int len;
-   char ch;
-   RECORD s_reco;
-   R_RECORD r_reco;
-   struct sockaddr_un address;
-   int result;
+   int server_sockfd, client_sockfd;
+   int server_len, client_len;
+   int req_no=0;
+   RECORD buf;
+   R_RECORD reco;
    
-   printf("\n Enter the Roll No.:");
-   scanf("%d",&s_reco.roll);
-   scanf("%c",&ch);
-   printf("\n Enter the Name:");
-   scanf("%[^\n]s",s_reco.name);
-   scanf("%c",&ch);
-   printf("\n Enter the CGPA:");
-   scanf("%f",&s_reco.cgpa);
- 
-   sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-   address.sun_family = AF_UNIX;
-   strcpy(address.sun_path, "server_socket");
-   len = sizeof(address);
-   result = connect(sockfd, (struct sockaddr *)&address, len);
-   if(result == -1) {
-      perror("oops: client1");
-      exit(1);
+   struct sockaddr_un server_address;
+   struct sockaddr_un client_address;
+   unlink("server_socket");
+   server_sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+   server_address.sun_family = AF_UNIX;
+   strcpy(server_address.sun_path, "server_socket");
+   server_len = sizeof(server_address);
+   bind(server_sockfd, (struct sockaddr *)&server_address, server_len);
+   listen(server_sockfd, 5);
+   
+   while(1) {
+      req_no++; 
+      printf("server waiting\n:");
+      client_len = sizeof(client_address);
+      client_sockfd = accept(server_sockfd,(struct sockaddr *)&client_address, &client_len);
+      read(client_sockfd, &buf, sizeof(buf));
+
+      reco.rec_no=req_no;
+      reco.roll=buf.roll;
+      strcpy(reco.name,buf.name);
+      reco.cgpa=buf.cgpa;
+
+      write(client_sockfd, &reco, sizeof(reco));
+      close(client_sockfd);
    }
-   write(sockfd, &s_reco, sizeof(s_reco));
-   read(sockfd, &r_reco, sizeof(r_reco));
-   printf("Record received from the server : \n Record No. %d \n %d, %s, %.2f\n",r_reco.rec_no, r_reco.roll, r_reco.name, r_reco.cgpa);
-   close(sockfd);
-   exit(0);
 }
